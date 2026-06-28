b90fb868e59d731bb6258d3a6fb9803a9b96ac88
nlowshii <luxxstl14@gmail.com>
2026-06-28 21:43:54 +0700

    Update CloseButtonMod.java

--- 26.1/src/main/java/id/nlw/Closebutton/CloseButtonMod.java
+++ 26.1/src/main/java/id/nlw/Closebutton/CloseButtonMod.java
@@ -1,27 +1,5 @@
-package id.nlowshii.closebutton;
-
-import net.fabricmc.api.ClientModInitializer;
-import net.fabricmc.fabric.api.client.screen.v1.ScreenEvents;
-import net.minecraft.client.gui.screens.inventory.AbstractContainerScreen;
-import net.minecraft.client.gui.components.Button;
-import net.minecraft.client.gui.components.Tooltip;
-import net.minecraft.network.chat.Component;
-
-public class CloseButtonMod implements ClientModInitializer {
-    @Override
-    public void onInitializeClient() {
-        ScreenEvents.AFTER_INIT.register((client, screen, scaledWidth, scaledHeight) -> {
-            if (!(screen instanceof AbstractContainerScreen<?> acs)) return;
-
-            int btnX = acs.leftPos + acs.imageWidth + 2;
-            int btnY = acs.topPos;
-
-            screen.addRenderableWidget(
-                Button.builder(Component.literal("✕"), btn -> client.setScreen(null))
-                    .bounds(btnX, btnY, 14, 14)
-                    .tooltip(Tooltip.create(Component.translatable("gui.close")))
-                    .build()
-            );
-        });
-    }
-}
+accessWidener v2 official
+accessible method net/minecraft/client/gui/screens/Screen addRenderableWidget (Lnet/minecraft/client/gui/components/events/GuiEventListener;)Lnet/minecraft/client/gui/components/events/GuiEventListener;
+accessible field net/minecraft/client/gui/screens/inventory/AbstractContainerScreen leftPos I
+accessible field net/minecraft/client/gui/screens/inventory/AbstractContainerScreen topPos I
+accessible field net/minecraft/client/gui/screens/inventory/AbstractContainerScreen imageWidth I
